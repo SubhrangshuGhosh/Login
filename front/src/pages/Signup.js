37192140e09d43eeab7edb08f1660ec628f0eabe
Subhrangshu Ghosh <ghoshayab@gmail.com>
2026-05-20 19:18:03 +0530

    Update signup API URL to production endpoint

--- front/src/pages/Signup.js
+++ front/src/pages/Signup.js
@@ -29,7 +29,7 @@ function Signup() {
             return handelError('All fields are required!')
         }
         try{
-            const url = "http://localhost:3000/auth/signup";
+            const url = "https://login-api-murex.vercel.app/auth/signup";
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
